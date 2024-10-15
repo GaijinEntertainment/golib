f92a43b835041c8c3f0dad54be5f9fb8b0ba22b1
a.zinoviev <a.zinoviev@gaijin.team>
2024-10-15 18:58:12 +0200

    [logrusadapter] implement basic adapter for logrus logger
    Change-Id: I7f8bbfd5c60786b8228a77667b7e12c0049684cd

---
+++ logger/logrusadapter/adapter.go
@@ -0,0 +1,89 @@
+package logrusadapter
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/GaijinEntertainment/golib/fields"
+	"github.com/GaijinEntertainment/golib/logger"
+)
+
+type logrusF func(*logrus.Entry, ...any)
+type logMapper [logger.LevelTrace + 1]logrusF
+
+//nolint:gochecknoglobals
+var mapper logMapper
+
+//nolint:gochecknoinits
+func init() {
+	mapper[logger.LevelError] = (*logrus.Entry).Error
+	mapper[logger.LevelWarning] = (*logrus.Entry).Warn
+	mapper[logger.LevelInfo] = (*logrus.Entry).Info
+	mapper[logger.LevelDebug] = (*logrus.Entry).Debug
+	mapper[logger.LevelTrace] = (*logrus.Entry).Trace
+}
+
+const LoggerNameKey = "logger"
+
+type Adapter struct {
+	lgr *logrus.Entry
+}
+
+func New(lgr *logrus.Entry) *Adapter {
+	return &Adapter{
+		lgr: lgr,
+	}
+}
+
+func (a *Adapter) Log(level int, msg string, err error, fs ...fields.Field) {
+	lgr := a.lgr
+
+	var lf logrusF
+
+	if level < len(mapper) {
+		lf = mapper[level]
+	}
+
+	if lf == nil {
+		lf = (*logrus.Entry).Info
+
+		lgr.WithField("got-level", level).Error("Unknown log level")
+	}
+
+	if err != nil {
+		lgr = lgr.WithError(err)
+	}
+
+	lf(lgr.WithFields(fieldsListToLogrusFields(fs)), msg)
+}
+
+func (a *Adapter) WithFields(fs ...fields.Field) logger.Adapter {
+	return &Adapter{
+		lgr: a.lgr.WithFields(fieldsListToLogrusFields(fs)),
+	}
+}
+
+// WithName returns a logger adapter with the given name attached to it. As
+// logrus does not support logger names, this adds [LoggerNameKey] field.
+func (a *Adapter) WithName(name string) logger.Adapter {
+	return &Adapter{
+		lgr: a.lgr.WithField(LoggerNameKey, name),
+	}
+}
+
+func (a *Adapter) WithStackTrace(_ string) logger.Adapter {
+	return a
+}
+
+func (*Adapter) Flush() error {
+	return nil
+}
+
+func fieldsListToLogrusFields(fs fields.List) logrus.Fields {
+	lfs := make(logrus.Fields, len(fs))
+
+	for _, f := range fs {
+		lfs[f.K] = f.V
+	}
+
+	return lfs
+}
